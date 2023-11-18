cd76d2419d975b9aaceae2db58f530b7b7899bb3
kai <kslaton@wisc.edu>
2023-11-17 19:58:02 -0600

    Simple Alarm Notification Working

--- app/src/main/java/com/cs407/oyasumi/AlarmActivity.java
+++ app/src/main/java/com/cs407/oyasumi/AlarmActivity.java
@@ -1,31 +1,76 @@
 package com.cs407.oyasumi;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class AlarmActivity extends AppCompatActivity {
+    private final ActivityResultLauncher<String> requestNotificationPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if(!isGranted) {
+                    //if the permission is refused, send a toast
+                    Toast.makeText(this, R.string.please_allow_alarm_notification, Toast.LENGTH_LONG).show();
+                }
+            });
+
+    private void requestNotificationPermission() {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            //notification permission not required till Android 13 (Tiramasu)
+            return;
+        }
+        //we check if the permission is granted already and if not, ask for permission using the
+        //static launcher
+        if(ContextCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.POST_NOTIFICATIONS
+        ) != PackageManager.PERMISSION_GRANTED) {
+            //permission not granted
+            requestNotificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+
+        requestNotificationPermission();
     }
 
+    public void saveAlarm(View view) {
+        TimePicker timePicker = (TimePicker) findViewById(R.id.alarmClock);
+        int hour = timePicker.getHour();
+        int minute = timePicker.getMinute();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("oyasumi_alarm_data", Context.MODE_PRIVATE);
+        sharedPreferences.edit().putInt("hour", hour).apply();
+        sharedPreferences.edit().putInt("minute", minute).apply();
+
+        Toast.makeText(this, "Alarm Saved", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 
     public void cancelAlarm(View view) {
-        SharedPreferences sharedPreferences = getSharedPreferences("alarm_data", Context.MODE_PRIVATE);
-        long hour = sharedPreferences.getLong("hour", -1);
-        long minute = sharedPreferences.getLong("minute", -1);
+        SharedPreferences sharedPreferences = getSharedPreferences("oyasumi_alarm_data", Context.MODE_PRIVATE);
+        int hour = sharedPreferences.getInt("hour", -1);
+        int minute = sharedPreferences.getInt("minute", -1);
         if ((hour == -1) || (minute == -1))
             openDialog();
         else {
@@ -34,19 +79,6 @@ public class AlarmActivity extends AppCompatActivity {
             startActivity(intent);
         }
     }
-
-    public void saveAlarm(View view) {
-        TimePicker timePicker = (TimePicker) findViewById(R.id.alarmClock);
-        long hour = timePicker.getHour();
-        long minute = timePicker.getMinute();
-        SharedPreferences sharedPreferences = getSharedPreferences("alarm_data", Context.MODE_PRIVATE);
-        sharedPreferences.edit().putLong("hour", hour).apply();
-        sharedPreferences.edit().putLong("minute", minute).apply();
-        Intent intent = new Intent(this, MainActivity.class);
-        Toast.makeText(this, "Alarm Saved", Toast.LENGTH_SHORT).show();
-        startActivity(intent);
-    }
-
 
     public void openDialog() {
         NoAlarmAlertDialog dialog = new NoAlarmAlertDialog();
--- app/src/main/java/com/cs407/oyasumi/MainActivity.java
+++ app/src/main/java/com/cs407/oyasumi/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,21 +13,26 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static AlarmManager alarmManager = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences sharedPreferences = getSharedPreferences("alarm_data", Context.MODE_PRIVATE);
-        long hour = sharedPreferences.getLong("hour", -1);
-        long minute = sharedPreferences.getLong("minute", -1);
+        //alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        SharedPreferences sharedPreferences = getSharedPreferences("oyasumi_alarm_data", Context.MODE_PRIVATE);
+        int hour = sharedPreferences.getInt("hour", -1);
+        int minute = sharedPreferences.getInt("minute", -1);
         if((hour == -1)||(minute == -1))
-            Log.i("Info", "alarm not set");
+            Log.i("Info", "MainActivity: alarm not set");
         else {
-            Log.i("Info", "alarm set");
-            startAlarm();
+            Log.i("Info", "MainActivity: alarm set");
+            setAlarm();
         }
     }
 
@@ -50,8 +56,28 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void startAlarm() {
-        //TODO will be implemented to start alarm service
-        //AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+    private void setAlarm() {
+        Log.i("info", "MainActivity: setting alarm");
+        SharedPreferences sharedPreferences = getSharedPreferences("oyasumi_alarm_data", Context.MODE_PRIVATE);
+        int hour = sharedPreferences.getInt("hour", -1);
+        int minute = sharedPreferences.getInt("minute", -1);
+        Log.i("info", "MainActivity: hour to be set - " + hour);
+        Log.i("info", "MainActivity: minute to be set - " + minute);
+
+        AlarmHelper.getInstance().createNotificationChannel(this);
+
+        AlarmHelper.getInstance().setCalendar(hour, minute);
+        Log.i("info", "MainActivity: setting alarm for: "  + AlarmHelper.getInstance().getTime());
+
+        Intent intentForReceiver = new Intent(this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 123, intentForReceiver, PendingIntent.FLAG_IMMUTABLE);
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancelAll();
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP,AlarmHelper.getInstance().getTime(), pendingIntent);
+        if(alarmManager.canScheduleExactAlarms())
+            Log.i("info", "MainActivity: can schedule exact alarms");
+        else
+            Log.i("info", "MainActivity: this is the problem");
+        Log.i("info", "MainActivity: alarm successfully saved");
     }
 }
